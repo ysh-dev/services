200019f50c28934f031cb93246a9a575d1f35343
HuHao <huhao@live.com>
2015-03-16 22:59:51 +0800

    added login & signin

--- js/ysh-data-api/routes/members.js
+++ js/ysh-data-api/routes/members.js
@@ -10,11 +10,44 @@ router.get('/', function(req, res, next) {
 			res.send({status : err});
 		}
 	});
-}).get('/dealers/rank/:number', function(req, res, next) {
+})
+.get('/dealers/rank/:number', function(req, res, next) {
 	var top = parseInt(req.params.number);
 	db.collection('members').find({type:'11'}).limit(top).sort({ta_compl_amount:-1}, function(err, docs) {
 		if(!err){
 			res.send(docs);		 
+		}else{
+			res.send({status : err});
+		}
+	});
+})
+.post('/account/', function(req, res, next){
+	db.collection('members').findOne({'login_name' : req.body.name}, function(err, doc){
+		if(!err){
+			if (doc){
+				res.send({'status' : 1, 'message' : 'member already exists'});
+			}else{
+				db.collection('members').insert({'login_name' : req.body.name, 'login_pass' : req.body.pass}, function(err, doc){
+					if (!err){
+						res.send({'status' : 0, 'result' : doc});	
+					}else{
+					    res.send({status : err});
+					}
+				});
+			}
+		}else{
+			res.send({status : err});
+		}
+	});
+}).get('/account/login', function(req,res, next){
+	var credential = {'login_name' : req.query.name, 'login_pass' : req.query.pass};
+	db.collection('members').findOne(credential, function(err, doc){
+		if(!err){
+			if (doc){
+				res.send({'status' : 0, 'result' : doc})
+			}else{
+				res.send({'status' : 1, 'message' : 'login failed'});
+			}			
 		}else{
 			res.send({status : err});
 		}
